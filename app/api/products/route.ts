54c06aba7ad1b82045c1c37f3a4ef5763bf9f888
Roman Fiala <fiala@vinaria.cz>
2025-02-15 12:45:27 +0100

    odstranění any route.ts 2

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
+import type { Product } from '@prisma/client';
 
 export async function GET() {
     try {
@@ -9,7 +10,7 @@ export async function GET() {
             }
         });
 
-        const serializedProducts = products.map(product => ({
+        const serializedProducts = products.map((product: Product) => ({
             ...product,
             id: product.id.toString()
         }));
