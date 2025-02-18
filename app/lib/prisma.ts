f49fc6646fcdc7084d6ebf7f21e0871dafb171e4
Roman Fiala <fiala@vinaria.cz>
2025-02-18 18:15:12 +0100

    odstranění any route.ts 10

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,16 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-// Deklarace pro globální prostor
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-// Vytvoření instance Prisma klienta
-const prisma = globalThis.prisma || new PrismaClient({
+const prisma = global.prisma || new PrismaClient({
   log: ['query', 'error', 'warn'],
 })
 
-// Nastavení globální instance v development módu
-if (process.env.NODE_ENV === 'development') globalThis.prisma = prisma
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
